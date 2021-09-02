c83c1ed75c650dfe958785744d82cbb13331fcac
unknown <mykola.shtompel@gmail.com>
2021-09-02 15:09:44 +0300

    New commit

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,12 +1,12 @@
 terraform {
   required_version = "= 1.0.2"
-  
+
   #S3 bucket for environment "dev"
   backend "s3" {
-    bucket  = "bucket-web-dev-nginx-us-east-2"
-    key     = "dev/terraform.tfstate"
-    region  = "us-east-2"
-    encrypt = true
+    bucket         = "bucket-web-dev-nginx-us-east-2"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-2"
+    encrypt        = true
     dynamodb_table = "table-web-dev-nginx-us-east-2"
   }
 
